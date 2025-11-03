cec2ea9a2e08074c43e552ba5776dc565e2f2d56
Adeoye Ibukunoluwa <adeoyeibukunoluwa3@gmail.com>
2025-11-03 16:33:10 +0100

    add volunteer section

---
+++ TEDx 2025/src/components/VolunteerSection.tsx
@@ -0,0 +1,9 @@
+export default function VolunteerSection() {
+  return <>
+    <section className="bg-gray-100 text-gray-800 px-6 md:px-24 py-12 w-full h-[50vh] border-b border-gray-300">
+      <div className="max-w-7xl mx-auto flex flex-col items-center text-center gap-8">
+        <h2 className="text-3xl md:text-4xl font-bold">Join Us as a Volunteer!</h2>
+      </div>
+    </section>
+  </>
+}
